b015c8ce24a5a77075049ad3eebb9d78e436a8a4
Guenaelle Deschamps <guenaelle.deschamps@gmail.com>
2017-09-11 19:47:38 +0200

    classe unite

--- src/main/java/entities/unite/Unite.java
+++ src/main/java/entities/unite/Unite.java
@@ -1,4 +1,15 @@
 package entities.unite;
 
+
 public abstract class Unite {
+
+    private int id;
+    private int healthPoint;
+    private int countOr;
+    private int countFer;
+    private int countPlutonium;
+    private long tempProduction;
+
+    
+
 }
