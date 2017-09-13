198c537b0dc926c733049578b0211db5b76d9312
yves-mael <yves-mael.bunetel@imie.fr>
2017-09-13 10:21:44 +0200

    commit planetMock

--- src/main/java/entities/Mock/PlanetMock.java
+++ src/main/java/entities/Mock/PlanetMock.java
@@ -1,8 +1,49 @@
 package entities.Mock;
 
+import entities.unite.Unite;
+import entities.unite.defense.ChampDeForce;
+import entities.unite.defense.Tourelle;
+import entities.unite.resource.Hangar;
 import entities.univers.Planete;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlanetMock {
-    Planete planete1 = new Planete();
-    Planete planete2 = new Planete();
+
+    public Planete planete1 = new Planete();
+    public Planete planete2 = new Planete();
+    List<Unite> unites = new ArrayList<>();
+    
+    PlanetMock(){
+        
+        unites.add(new Hangar());
+
+        planete1.setId(1);
+        planete1.setChampForce(false);
+        planete1.setCoordx(2);
+        planete1.setCoordx(4);
+        planete1.setFer(100);
+        planete1.setOr(100);
+        planete1.setPlutonium(100);
+        planete1.setPlanetSize(50);
+        planete1.setOccuped(true);
+        planete1.setUnites(unites);
+
+        unites.add(new Tourelle());
+        unites.add(new ChampDeForce());
+
+        planete2.setId(2);
+        planete2.setChampForce(true);
+        planete2.setCoordx(2);
+        planete2.setCoordx(4);
+        planete2.setFer(100);
+        planete2.setOr(100);
+        planete2.setPlutonium(100);
+        planete2.setPlanetSize(75);
+        planete2.setOccuped(true);
+        planete2.setUnites(unites);
+
+    }
+
 }
