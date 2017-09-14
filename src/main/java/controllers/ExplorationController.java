12856540891673374ea186fb2d19db4e084227a1
yves-mael <yves-mael.bunetel@imie.fr>
2017-09-14 11:31:36 +0200

    commit bootstrap.cdn added to templates

--- src/main/java/controllers/ExplorationController.java
+++ src/main/java/controllers/ExplorationController.java
@@ -1,15 +1,32 @@
 package controllers;
 
+import entities.univers.Planete;
+import entities.univers.Univers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import services.ExplorationService;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class ExplorationController {
 
-    @RequestMapping(value = "/universe/planet/exploration", method = RequestMethod.POST)
-    public String checkDistance (Model model){
+    @Autowired
+    private ExplorationService explorationService;
+
+    @RequestMapping(value = "/universe/planet/exploration", method = RequestMethod.GET)
+    public String checkDistance (Model model, HttpSession session){
+
+        List<Planete> planeteList = explorationService.getNotOwnedPlanetes(
+                (Univers) session.getAttribute("univers"),
+                (int) session.getAttribute("playerId")
+        );
+
+        model.addAttribute("planetes" planeteList);
         return "exploration";
     }
 
--- src/main/java/services/ExplorationService.java
+++ src/main/java/services/ExplorationService.java
@@ -1,9 +1,27 @@
 package services;
 
 
+import entities.univers.Planete;
+import entities.univers.Univers;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ExplorationService {
 
+    public List<Planete> getNotOwnedPlanetes(Univers univers,int id){
+
+        List<Planete> planetes = univers.getPlanetes();
+        List<Planete> notOwnedPlanetes = new ArrayList<>();
+
+        for (Planete planete : planetes
+                ) {
+            if (planete.getPlayerId() != id){
+                notOwnedPlanetes.add(planete);
+            }
+        }
+        return planetes;
+    }
 }
