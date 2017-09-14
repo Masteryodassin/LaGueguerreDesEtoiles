ba52d989d7b6c8a1fdc540d4bd34162ae6cf11ae
Guenaelle <guenaelle.deschamps@gmail.com>
2017-09-14 16:16:19 +0200

    ajout de trucs...

--- src/main/java/controllers/PlanetManagementController.java
+++ src/main/java/controllers/PlanetManagementController.java
@@ -4,6 +4,8 @@ package controllers;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import entities.Mock.PlanetMock;
 import entities.unite.Unite;
+import entities.unite.orbitale.UniteOrbitale;
+import entities.unite.resource.Hangar;
 import entities.univers.Joueur;
 import entities.univers.Planete;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +19,22 @@ import services.PlanetManagementService;
 import services.PlanetService;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Controller
 public class PlanetManagementController {
 
-    private PlanetManagementService planetManagementService;
+   private Hangar hangar;
 
     @Autowired
     private PlanetService planetService;
+
+    @Autowired
+    private PlanetManagementService planetManagementService;
 
     @RequestMapping(value = "/universe/planet/planetManagement", method = RequestMethod.GET)
     public String getUnite (Model model, HttpSession session){
@@ -35,7 +43,12 @@ public class PlanetManagementController {
 
         Planete planete = planetService.getById(planetId, joueur);
         model.addAttribute("planete", planete);
+
         List<Unite> unites = planete.getUnites();
+        model.addAttribute("unites", unites);
+
+       // Map<String, Integer> ressources = planetManagementService.getRessources();
+       // model.addAttribute("ressources", ressources);
 
         return "planetManagement";
 
@@ -51,6 +64,7 @@ public class PlanetManagementController {
     public String buildUnit(Model model){
         return "planetManagement";
     }
+
 
     @RequestMapping(value = "/universe/planet", method = RequestMethod.DELETE)
     public String deleteUnit(Model model){
--- src/main/java/services/PlanetManagementService.java
+++ src/main/java/services/PlanetManagementService.java
@@ -2,21 +2,36 @@ package services;
 
 import entities.Mock.PlanetMock;
 import entities.unite.Unite;
+import entities.unite.resource.Hangar;
 import entities.univers.Planete;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
-public class PlanetManagementService {
+public class PlanetManagementService <T extends Unite>{
 
     private Planete planete;
+    private Unite unite;
 
 
-
-    public boolean canBuid (){
-    return true;
+    public int getAvailableSpace(){
+        List<Unite> unites = planete.getUnites();
+        int space = planete.getPlanetSize()-unites.size();
+        return space;
     }
+
+    public boolean createUnite(T unite){
+       return true;
+    }
+
+    public Unite deleteUnite(){
+        return null;
+    }
+
+
 
 }
